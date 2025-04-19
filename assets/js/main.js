0a96670c4b3ce16f73f0d3f7651484e1b13a2ad5
Denis <d_berezuk@icloud.com>
2025-04-20 02:08:58 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+new Swiper('.villa__slider', {
+	slidesPerView: 2,
+	spaceBetween: 13,
+
+	navigation: {
+		nextEl: '.villa__slider-button.swiper-button-next',
+		prevEl: '.villa__slider-button.swiper-button-prev',
+	},
+
+	breakpoints: {
+		824: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+});
+
+const buttonsForPopUpOpen = document.querySelectorAll('.button[data-pop-up]'),
+	buttonBurgerMenu = document.querySelector('.header__button-burger');
+(popUp = document.querySelector('.pop-up')),
+	(menuOverlay = popUp.querySelector('.menu-overlay')),
+	(registerYourInterestForm = popUp.querySelector('.register-interests-form')),
+	(popUpButtonClose = document.querySelector('.pop-up__button-close'));
+
+function onClosingBlocksPopUp() {
+	const blocks = popUp.querySelectorAll('.pop-up__blocks > *');
+
+	blocks.forEach(block => block.classList.add('pop-up__block-hidden'));
+}
+
+function onOpenPopUp(event) {
+	event.stopPropagation();
+
+	document.body.style.overflow = 'hidden';
+
+	onClosingBlocksPopUp();
+
+	popUp.classList.add('pop-up--active');
+}
+
+function onClosePopUp() {
+	onClosingBlocksPopUp();
+
+	document.body.style.overflow = '';
+
+	popUp.classList.remove('pop-up--active');
+}
+
+function onClosingPopUpWithoutClick(event) {
+	if (!event.target.closest('.pop-up__body')) {
+		onClosingBlocksPopUp();
+
+		document.body.style.overflow = '';
+
+		onClosePopUp();
+	}
+}
+
+function onOpenPopUpRegisterYourInterestForm(event) {
+	onOpenPopUp(event);
+
+	registerYourInterestForm.classList.remove('pop-up__block-hidden');
+}
+
+function onOpenPopUpMobileOverlay(event) {
+	onOpenPopUp(event);
+
+	menuOverlay.classList.remove('pop-up__block-hidden');
+}
+
+buttonsForPopUpOpen.forEach(button => {
+	button.addEventListener('click', onOpenPopUpRegisterYourInterestForm);
+});
+
+buttonBurgerMenu.addEventListener('click', onOpenPopUpMobileOverlay);
+
+popUpButtonClose.addEventListener('click', onClosePopUp);
+
+document.addEventListener('click', onClosingPopUpWithoutClick);
